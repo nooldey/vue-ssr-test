20a358695255e8e6ad7bc5a0cded989d32db06d2
nooldey <openzws@163.com>
2017-07-26 18:56:25 +0800

    add router and store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-      HELLO Nooldey
+      <header>
+          <a href="#">github</a>
+          <!-- <nav>
+              <router-link to="/" exact>
+                撒啊
+              </router-link>
+              <router-link to=""></router-link>
+              <router-link to=""></router-link>
+              <router-link to=""></router-link>
+          </nav> -->
+      </header>
   </div>
 </template>
 
